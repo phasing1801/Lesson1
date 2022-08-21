65e11539aa6ff88d034ec5f9a1c8ba94bb9e5778
phasing1801 <108165997+phasing1801@users.noreply.github.com>
2022-08-22 04:20:18 +0500

    Добавьте файлы проекта.

---
+++ Homework/Task1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите ваше имя.");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Напишите вашу фамилию.");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Укажите ваш возраст.");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Укажите ваш вес.");
+            string weight = Console.ReadLine();
+
+
+            Console.WriteLine("Ваше имя - " + name + ", ваша фамилия - " + surname + ", ваш возраст - " + age + ", ваш вес - " + weight + ". Ваша анкета готова!");
+
+            Console.WriteLine("Ваше имя - {0}, ваша фамилия - {1}, ваш возраст - {2}, ваш вес - {3}. Ваша анкета готова!", name, surname, age, weight);
+            Console.WriteLine($"Ваше имя - {name}, ваша фамилия - {surname}, ваш возраст - {age}, ваш вес - {weight}. Ваша анкета готова!"); 
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ Homework/Task2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для вычисления индекса массы тела (ИМТ), введите следующие данные:");
+
+            Console.WriteLine("Введдите вашу массу в килограммах:");
+            string mass = Console.ReadLine();
+
+            int m = int.Parse(mass);
+
+            Console.WriteLine("Введите ваш рост в метрах:");
+            string height = Console.ReadLine();
+
+            int h = int.Parse(height);
+
+
+            int IMB = CalcIMB(m, h);
+
+            Console.WriteLine($"Ваш ИМТ равен: {IMB}");
+            Console.ReadLine();
+        }
+
+        static int CalcIMB(int m, int h)
+        {
+            int res = h / m * m;
+
+            return res;
+        }
+    }
+
+}
